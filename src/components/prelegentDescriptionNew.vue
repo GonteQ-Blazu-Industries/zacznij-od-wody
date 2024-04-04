770973d412d856f87cbee972450c8ad9b28b0f08
JoannaBilska <asiabilska46@gmail.com>
2024-04-04 22:10:09 +0200

    New meeting

---
+++ src/components/prelegentDescriptionNew.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="prelegent-description-wrapper">
+        <img v-if="image && image.trim()" :src="imagePath" alt="zdjęcie prelegenta">
+        <div class="prelegent-info-wrapper">
+            <h3>{{ name }}</h3>
+            <p>{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['image', 'name', 'description'],
+    data() {
+        return {
+            imagePath: null,
+        };
+    },
+    async mounted() {
+        await this.loadImage();
+    },
+    watch: {
+        image: 'loadImage',
+    },
+    methods: {
+        async loadImage() {
+            if (!this.image || !this.image.trim()) {
+                this.imagePath = null;
+                return;
+            }
+            try {
+                const imageModule = await import(`@/assets/${this.image}`);
+                this.imagePath = imageModule.default;
+            } catch (error) {
+                console.error(`Błąd ładowania obrazu: ${error}`);
+                this.imagePath = null;
+            }
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.prelegent-description-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    width: 320px;
+    @media only screen and (max-width: 720px) {
+        width: 300px;
+        gap: .5rem;
+    }
+    img{
+        aspect-ratio: 1/1;
+        height: 320px;
+        object-fit: cover;
+    }
+
+    .prelegent-info-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        font-family: 'Poppins', sans-serif;
+        color: #183a37;
+        gap: 1rem;
+
+        h3 {
+            font-weight: 700;
+            font-size: 1.2rem;
+            @media only screen and (max-width: 720px) {
+                font-size: 1.2rem;
+            }
+        }
+
+        p {
+            text-align: left;
+            font-size: 1rem;
+            font-weight: 200;
+            @media only screen and (max-width: 720px) {
+                font-size: 0.9rem;
+                color: #000000;
+                font-weight: 500;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/theMeetingNavbarNew.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="the-meeting-navbar-wrapper">
+        <router-link to="/">
+            <img src="../assets/pabilkropla.png" alt="Strona główna" height="50"></router-link>
+        <ul class="the-meeting-navbar">
+            <a class="the-meeting-navbar-item" href="#about-meeting"  v-smooth-scroll="{duration: 1000, offset: -100, updateHistory: false,}">O wydarzeniu</a>
+            <a class="the-meeting-navbar-item" href="#leaders-description"  v-smooth-scroll="{duration: 1000, offset: -100, updateHistory: false,}">Prelegenci</a>
+            <a class="the-meeting-navbar-item" href="#program-info"  v-smooth-scroll="{duration: 1000, offset: -100, updateHistory: false,}">Program</a>
+            <a class="the-meeting-navbar-item" href="#date-and-place"  v-smooth-scroll="{duration: 1000, offset: -100, updateHistory: false,}">Data i miejsce</a>
+            <a class="the-meeting-navbar-item" href="#contact-info"  v-smooth-scroll="{duration: 1000, offset: -100, updateHistory: false,}">Kontakt</a>
+            <a class="the-meeting-navbar-item-buy" href="#buy-ticket"  v-smooth-scroll="{duration: 1000, offset: -100, updateHistory: false,}">Kup bilet</a>
+            <!-- <li class="the-meeting-navbar-item buy-ticket" @click="$router.push({ name: 'RzemienBuyTicket' })">Kup bilet</li> -->
+        </ul>
+        <div class="mobile-menu-icon-wrapper" @click="mobileMenuActive = !mobileMenuActive">
+            <a class="the-meeting-navbar-item-buy" href="#buy-ticket"  v-smooth-scroll="{duration: 1000, offset: -120, updateHistory: false,}">Kup bilet</a>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.the-meeting-navbar-wrapper{
+    background-color: white;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2vh 5vw;
+    img{
+        @media only screen and (max-width: 720px) {
+            height: 30px;
+        }
+    }
+    .the-meeting-navbar{
+        list-style-type: none;
+        display: flex;
+        gap: 1.2rem;
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.2rem;
+        align-items: center;
+        a{
+            text-decoration: none;
+            color: #183A37;
+        }
+        .the-meeting-navbar-item{
+            padding: .5rem 1rem;
+            &:hover{
+                cursor: pointer;
+            }
+        }
+        .the-meeting-navbar-item-buy{
+            color: white;
+            background-color: #183A37;
+            border-radius: 5px;
+            padding: .5rem 1rem;
+
+            &:hover{
+                background-color: #183A37;
+                // border: 2px solid #0B3954;
+                // border-style: inset;
+                box-shadow: 5px 5px 15px #183A37 ;
+                border-radius: 5px;
+                color: white;
+                cursor: pointer;
+            }
+        }
+
+        @media only screen and (max-width: 720px){
+            display: none;
+        }
+    }
+    .mobile-menu-icon-wrapper{
+        display: none;
+
+        @media only screen and (max-width: 720px) {
+            display: block;
+        }
+    }
+}
+</style>
